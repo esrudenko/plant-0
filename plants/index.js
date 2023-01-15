7f02a52ee6aeeb2c82909450b73f9922d17e8481
Elena Mankerova <esrudenko93@gmail.com>
2023-01-16 03:57:47 +0600

    docx: change closing hamburger-menu func

--- plants/index.js
+++ plants/index.js
@@ -1,44 +1,18 @@
-/*function burgerMenu(selector) {
-let menu = $(selector);
-let button = menu.find('.burger-menu-btn');
-let links = menu.find('.burger-menu-link');
-let overlay = menu.find('.burger-menu-overlay');
+let hamb = document.querySelector(".hamb");
+let navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+let navOverlay = document.querySelector(".navbar-overlay")
 
-button.on('click', (e) => {
-	e.preventDefault();
-	toggleMenu();
-});
-
-links.on('click', () => toggleMenu());
-overlay.on('click', () => toggleMenu());
-
-function toggleMenu() {
-menu.toggleClass('.burger-menu-active')
-if (menu.hasClass('.burger-menu-active')) {
-	$('body').css('overflow', 'hidden');
-} else {
-	$('body').css('overflow', 'visible');
-}
-}
-}
-burgerMenu('.burger-menu');
-
-*/
-
-  let hamb = document.querySelector(".hamb");
-  let navMenu = document.querySelector(".nav-menu");
-  const navLink = document.querySelectorAll(".nav-link");
-  let navOverlay = document.querySelector(".navbar-overlay")
-
-  hamb.addEventListener("click", mobileMenu);
-  function mobileMenu() {
+hamb.addEventListener("click", mobileMenu);
+function mobileMenu() {
 	hamb.classList.toggle("active");
 	navMenu.classList.toggle("active");
-  }
- navLink.forEach(n => n.addEventListener("click", closeMenu));
- navOverlay.addEventListener("click", closeMenu);
- function closeMenu() {
+	navOverlay.classList.toggle("active");
+}
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+navOverlay.addEventListener("click", closeMenu);
+function closeMenu() {
 	hamb.classList.remove("active");
 	navMenu.classList.remove("active");
-
- }
+	navOverlay.classList.remove("active");
+}
